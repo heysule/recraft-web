ba221f409812bd9a42ccdc7bf22d8173bb9e2f22
Suleiman Yunus <suleiman.yunus@meltwater.org>
2023-01-16 14:06:40 +0000

    chore: abstract sign in modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MobileNavComponent } from './common/components/mobile-nav/mobile-nav.co
 import { ProductItemComponent } from './features/products/product-list/product-item/product-item.component';
 import { NotFoundComponent } from './common/components/not-found/not-found.component';
 import { FooterComponent } from './common/components/footer/footer.component';
+import { SignInModalComponent } from './common/components/sign-in-modal/sign-in-modal.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { FooterComponent } from './common/components/footer/footer.component';
     MobileNavComponent,
     ProductItemComponent,
     NotFoundComponent,
-    FooterComponent
+    FooterComponent,
+    SignInModalComponent
   ],
   imports: [
     BrowserModule,
